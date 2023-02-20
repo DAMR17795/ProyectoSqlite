e630c61938d61960b1ef250a36f6267017c12d58
Daniel <dmarrom577@g.educaand.es>
2023-02-20 23:24:43 +0100

    instituto

--- app/src/main/java/www/iesmurgi/proyectosqlite/actividades/Splash.kt
+++ app/src/main/java/www/iesmurgi/proyectosqlite/actividades/Splash.kt
@@ -10,5 +10,6 @@ class Splash:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
